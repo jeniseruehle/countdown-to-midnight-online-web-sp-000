8ac45f568a65182e3638a5f5e637095fd843fad4
Jenise Ruehle <jela07@gmail.com>
2020-01-28 21:18:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(number)
   number = 10 
-  while number < 10
+  while number <= 10
     puts "#{number} SECOND(S)!"
     number -= 1 
   end
@@ -10,4 +10,5 @@ def countdown(number)
 end 
 
 def countdown_with_sleep(number)
+end
   
